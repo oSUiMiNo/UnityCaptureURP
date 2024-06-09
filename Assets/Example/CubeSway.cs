9df0574cff823bf365646538f0cf980e2a32d196
t170d025@gunma-u.ac.jp <t170d025@gunma-u.ac.jp>
2024-06-09 18:37:46 +0900

    First. Only Sample Project.

---
+++ Assets/Example/CubeSway.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class CubeSway : MonoBehaviour
+{
+
+    float RotationAmount = 6f, RotationSpeedX = 2.5f, RotationSpeedY = 1.75f;
+    GameObject cube;
+
+    private void Awake()
+    {
+        Debug.Log($"1");
+    }
+
+    private void Start()
+    {
+        Debug.Log($"2");
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.position = transform.position + new Vector3(0, 0, 3);
+    }
+
+    private void Update()
+    {
+        Debug.Log($"3");
+    }
+
+    void FixedUpdate()
+    {
+        cube.transform.Rotate(new Vector3(1, 1, 1));
+
+        transform.localRotation = Quaternion.Euler(new Vector3(Mathf.Cos(Time.time * RotationSpeedX), Mathf.Sin(Time.time * RotationSpeedY)) * RotationAmount);
+    }
+}
